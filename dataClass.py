bc0666a89763ce86cbc6c21a1e5303ab68ddd552
Вадим Воробьев <vadimvorobev@Vadims-MacBook-Pro.local>
2022-01-26 17:38:03 +0300

    dataClass

---
+++ dataClass.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+from torch.utils.data import Dataset
+import os
+
+class dataClass(Dataset):
+    def __init__(self, img_labels, img_dir, transform=None):
+        super().__init__()
+        self.img_labels = img_labels
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return self.img_labels.shape[0]
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[index, 0] + '.tif')
+        image = cv.imread(img_path)
+        label = self.img_labels.iloc[index, 1]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
---
+++ main.py
@@ -0,0 +1,11 @@
+from dataClass import dataClass
+import pandas as pd
+import cv2 as cv
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+import numpy as np
+import os
+
+
